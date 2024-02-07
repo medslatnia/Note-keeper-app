22620b7cee0a2dd285d52e5c4fecb73f1772d4af
Mohammed SLATNIA <slatnia.mmed@gmail.com>
2024-02-07 17:51:59 +0100

    Note Keeper mobile application

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -5,9 +5,9 @@ class Note{
   String _date;
   int _priority;
 
-  Note(this._title, this._date, this._priority, [this._description]);
+  Note(this._title, this._date, this._priority, this._description);
 
-  Note.withId(this._id,this._title, this._date, this._priority, [this._description]);
+  Note.withId(this._id,this._title, this._date, this._priority, this._description);
 
   int get id => _id;
   String get title => _title;
@@ -40,7 +40,7 @@ class Note{
   //convert a note object into a map object
 Map<String, dynamic> toMap(){
    var map = Map<String, dynamic>();
-   if(id!=null){
+   if(_id!=null){
      map['id']=_id;
   }
    map['id']=_id;
